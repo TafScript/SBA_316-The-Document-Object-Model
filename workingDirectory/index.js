cd7c123ab1971fd0932ca23cb3dd6e6330656dc8
Tafari <Tafarigeorge5079@gmail.com>
2025-09-19 19:22:06 -0400

    Handle function added, modified updateClock function

--- workingDirectory/index.js
+++ workingDirectory/index.js
@@ -1,11 +1,19 @@
 let user = prompt("Enter your name:");
-
 document.getElementById("greeting").textContent = "Welcome, " + user + "!";
 
-
 function updateClock() {
-  const now = new Date();
-  document.getElementById("clock").textContent = now.toLocaleTimeString();
+  document.getElementById("clock").textContent = new Date().toLocaleTimeString();
 }
 setInterval(updateClock, 1000);
 updateClock();
+
+function handleSubmit(e) {
+  e.preventDefault();
+  alert(
+    "Name: " + name.value + 
+    "\nEmail: " + email.value + 
+    "\nMessage: " + message.value
+  );
+}
+
+document.getElementById("form").onsubmit = handleSubmit;
